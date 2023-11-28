d9d9fc0e0c702acc17fdd2eeb606433c3408ed9a
Moises Ubaldo <ubaldolifes724@gmail.com>
2023-11-27 18:47:12 -0600

    Proyecto Java

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyecto de Universidad 
+
+Un sistema de veneta de un restaurante de 
+hamburguesas y conectarla en una bd local
+
+![principal](1.PNG)
+
---
+++ bd/127_0_0_1.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-11-2023 a las 08:51:36
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login`
+--
+CREATE DATABASE IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `login`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+-- Creación: 12-11-2023 a las 07:49:30
+-- Última actualización: 12-11-2023 a las 07:50:47
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `ingresoUsuario` varchar(70) DEFAULT NULL,
+  `ingresoContrasenia` varchar(70) DEFAULT NULL,
+  `edad` int(50) DEFAULT NULL,
+  `sexo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `ingresoUsuario`, `ingresoContrasenia`, `edad`, `sexo`) VALUES
+(1, 'juan', '123', 17, 'hombre');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
